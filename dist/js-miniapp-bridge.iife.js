216dd442cfa7ca3824ac27f7b91be516c5eceecf
Mirzo Khushang <mirzokhushang@Mirzos-MacBook-Pro.local>
2023-01-17 10:15:26 +0500

    +

---
+++ dist/js-miniapp-bridge.iife.js
@@ -0,0 +1,1 @@
+var jsMiniappBridge=function(e){"use strict";const n="",t=(u,i)=>u+i;return e.sum=t,Object.defineProperties(e,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}}),e}({});
--- dist/js-miniapp-sdk.iife.js
+++
@@ -1,1 +0,0 @@
-var jsMiniappSdk=function(e){"use strict";const s="",t=(u,n)=>u+n;return e.sum=t,Object.defineProperties(e,{__esModule:{value:!0},[Symbol.toStringTag]:{value:"Module"}}),e}({});
